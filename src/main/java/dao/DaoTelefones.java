5214aef8e4b83c7eee5f8b1ad49a8e119c32a6cc
flavio salgado <flaviosalgado2@hotmail.com>
2019-10-22 17:27:17 -0300

    novas classes e paginas

---
+++ src/main/java/dao/DaoTelefones.java
@@ -0,0 +1,9 @@
+package dao;
+
+import model.TelefoneUser;
+
+public class DaoTelefones<E> extends DaoGeneric<TelefoneUser> {
+
+	private static final long serialVersionUID = 8326767772383980759L;
+	
+}
