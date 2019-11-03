767c49fa442976c2b8cc42a8669b0c85629acc7b
flavio salgado <flaviosalgado2@hotmail.com>
2019-11-03 14:56:48 -0300

    implementacao do datalazy

--- src/main/java/managedBean/UsuarioPessoaManagedBean.java
+++ src/main/java/managedBean/UsuarioPessoaManagedBean.java
@@ -1,13 +1,13 @@
 package managedBean;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -15,8 +15,10 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.AjaxBehaviorEvent;
+import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.DatatypeConverter;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.ChartSeries;
@@ -25,6 +27,7 @@ import com.google.gson.Gson;
 
 import dao.DaoEmail;
 import dao.DaoUsuario;
+import datatablelazy.LazyDataTableModelUserPessoa;
 import model.EmailUser;
 import model.UsuarioPessoa;
 
@@ -35,7 +38,7 @@ public class UsuarioPessoaManagedBean implements Serializable {
 	private static final long serialVersionUID = 9084995168573531022L;
 
 	private UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
-	private List<UsuarioPessoa> list = new ArrayList<UsuarioPessoa>();
+	private LazyDataTableModelUserPessoa<UsuarioPessoa> list = new LazyDataTableModelUserPessoa<UsuarioPessoa>();
 	private DaoUsuario<UsuarioPessoa> daoGeneric = new DaoUsuario<UsuarioPessoa>();
 
 	private BarChartModel barChartModel = new BarChartModel();
@@ -44,11 +47,12 @@ public class UsuarioPessoaManagedBean implements Serializable {
 	private DaoEmail<EmailUser> daoEmail = new DaoEmail<EmailUser>();
 
 	private String campoPesquisa;
-	
+
 	@PostConstruct
 	public void init() {
 		
-		list = daoGeneric.listar(UsuarioPessoa.class);
+		list.load(0, 5, null, null, null);
+		//list = daoGeneric.listar(UsuarioPessoa.class);
 		montarGrafico();
 
 	}
@@ -58,7 +62,7 @@ public class UsuarioPessoaManagedBean implements Serializable {
 
 		ChartSeries userSalario = new ChartSeries();
 
-		for (UsuarioPessoa usuarioPessoa : list) {
+		for (UsuarioPessoa usuarioPessoa : list.list) {
 
 			userSalario.set(usuarioPessoa.getNome(), usuarioPessoa.getSalario());
 
@@ -112,7 +116,7 @@ public class UsuarioPessoaManagedBean implements Serializable {
 	public String salvar() {
 
 		daoGeneric.salvar(usuarioPessoa);
-		list.add(usuarioPessoa);
+		list.list.add(usuarioPessoa);
 		usuarioPessoa = new UsuarioPessoa();
 		init();
 		FacesContext.getCurrentInstance().addMessage(null,
@@ -125,7 +129,7 @@ public class UsuarioPessoaManagedBean implements Serializable {
 		return "";
 	}
 
-	public List<UsuarioPessoa> getList() {
+	public LazyDataTableModelUserPessoa<UsuarioPessoa> getList() {
 
 		return list;
 	}
@@ -135,7 +139,7 @@ public class UsuarioPessoaManagedBean implements Serializable {
 		try {
 
 			daoGeneric.removerUsuario(usuarioPessoa);
-			list.remove(usuarioPessoa);
+			list.list.remove(usuarioPessoa);
 			usuarioPessoa = new UsuarioPessoa();
 			init();
 			FacesContext.getCurrentInstance().addMessage(null,
@@ -192,18 +196,39 @@ public class UsuarioPessoaManagedBean implements Serializable {
 	public String getCampoPesquisa() {
 		return campoPesquisa;
 	}
-	
+
 	public void pesquisar() {
-		list = daoGeneric.pesquisar(campoPesquisa);
 		
+		list.pesquisar(campoPesquisa);
 		montarGrafico();
 	}
-	
-	public void upload (FileUploadEvent image) {
-		
+
+	public void upload(FileUploadEvent image) {
+
 		String imagem = "data:image/png;base64," + DatatypeConverter.printBase64Binary(image.getFile().getContents());
 		usuarioPessoa.setImagem(imagem);
-		
+
+	}
+
+	public void download() throws IOException {
+		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		String fileDownloadID = params.get("fileDownload");
+		UsuarioPessoa pessoa = daoGeneric.pesquisar(Long.parseLong(fileDownloadID), UsuarioPessoa.class);
+
+		byte[] imagem = new Base64().decodeBase64(pessoa.getImagem().split("\\,")[1]);
+
+		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext()
+				.getResponse();
+
+		response.addHeader("Content-Disposition", "attachment; filename=download.png");
+
+		response.setContentType("application/octet-stream");
+
+		response.setContentLength(imagem.length);
+		response.getOutputStream().write(imagem);
+		response.getOutputStream().flush();
+		FacesContext.getCurrentInstance().getResponseComplete();
+
 	}
 
 }
